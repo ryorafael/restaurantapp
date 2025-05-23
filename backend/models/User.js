1d7084fd15880071dde2dfd8dd0f347fbc87c396
Ryo Rafael <ryorafael18@gmail.com>
2025-05-23 15:36:33 +0200

    clean up

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,3 @@
-// const { DataTypes } = require("sequelize");
-// const sequelize = require("../config/sequelize");
-
-// const User = sequelize.define("User", {
-//   name: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   email: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//     unique: true,
-//   },
-//   password: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   role: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//     defaultValue: "user", // Default role is 'user'
-//   },
-// });
-
-// module.exports = User;
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     name: {
